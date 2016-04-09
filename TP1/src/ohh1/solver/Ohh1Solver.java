3bcabff0387de701f16c55039e949fea3d6a0cd3
Florencia Besteiro <florbest89@gmail.com>
2016-04-09 20:08:04 -0300

    no message

--- TP1/src/ohh1/solver/Ohh1Solver.java
+++ TP1/src/ohh1/solver/Ohh1Solver.java
@@ -17,14 +17,29 @@ public class Ohh1Solver {
 		if(args.length == 3){
 			try{
 				int boardSize = Integer.parseInt(args[2]);
+				
 				if(boardSize == 4 || boardSize == 6 || boardSize == 8){
 					Ohh1State.setSize(boardSize);
 					System.out.println("Game board size: " + boardSize + "x" + boardSize);
+				} else {
+					System.out.println("Invalid size! Size must be '4', '6' or '8'");
+					return;
 				}
-			}catch(NumberFormatException ex){
 				
+			}catch(NumberFormatException ex){
+				System.out.println("Invalid arguments! Size must be a number");
+				return;
 			}
+		} else {
+			System.out.println("Invalid arguments!");
+			return;
 		}
+		
+		String heuristic = args[0];
+		String searchStrategy = args[1];
+		
+		
+		
 	}
 
 }
