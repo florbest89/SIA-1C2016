0dbb48cb0bc4338af00a57a51dc0401fed05858b
Florencia Besteiro <florbest89@gmail.com>
2016-04-09 22:50:55 -0300

    Solver terminado

--- TP1/src/ohh1/solver/Ohh1Solver.java
+++ TP1/src/ohh1/solver/Ohh1Solver.java
@@ -1,5 +1,10 @@
 package ohh1.solver;
 
+import gps.GPSEngine;
+import gps.SearchStrategy;
+import gps.api.GPSProblem;
+import ohh1.gps.api.Ohh1Engine;
+import ohh1.gps.api.Ohh1Problem;
 import ohh1.gps.api.Ohh1State;
 
 public class Ohh1Solver {
@@ -32,12 +37,53 @@ public class Ohh1Solver {
 			}
 		} else {
 			System.out.println("Invalid arguments!");
+			System.out.println("Arguments must be (in order) : Heuristic, Search strategy, Board size");
+			System.out.println("Heuristics available: SwapsPerRows, SwapsPerCols");
+			System.out.println("Search strategies available: IDDFS, BFS, DFS, ASTAR, GREEDY");
+			System.out.println("Boards of size 4, 6 or 8 only");
 			return;
 		}
 		
 		String heuristic = args[0];
+		
+		if(heuristic.equalsIgnoreCase("swapsperrow")){
+			//TODO : Seleccionar el enum que corresponda a esta heuristica			
+		} else if( heuristic.equalsIgnoreCase("swapspercol")){
+			//TODO : Idem a arriba
+		} else {
+			System.out.println("Invalid arguments! Heuristics must be 'SwapsPerRow' or 'SwapsPerCol'");
+			return;
+		}
+		
+		GPSProblem ohh1Problem = new Ohh1Problem();
+		GPSEngine solver = new Ohh1Engine();
+		
 		String searchStrategy = args[1];
 		
+		if(searchStrategy.equalsIgnoreCase(SearchStrategy.IDDFS.toString())){
+			System.out.println("Starting engine with search strategy: " + "Iterative");
+			solver.engine(ohh1Problem, SearchStrategy.IDDFS);
+		}
+		
+		if(searchStrategy.equalsIgnoreCase(SearchStrategy.BFS.toString())){
+			System.out.println("Starting engine with search strategy: " + "BFS");
+			solver.engine(ohh1Problem, SearchStrategy.BFS);
+		}
+		
+		if(searchStrategy.equalsIgnoreCase(SearchStrategy.DFS.toString())){
+			System.out.println("Starting engine with search strategy: " + "DFS");
+			solver.engine(ohh1Problem, SearchStrategy.DFS);
+		}
+		
+		if(searchStrategy.equalsIgnoreCase(SearchStrategy.ASTAR.toString())){
+			System.out.println("Starting engine with search strategy: " + "A*");
+			solver.engine(ohh1Problem, SearchStrategy.ASTAR);
+		}
+		
+		if(searchStrategy.equalsIgnoreCase(SearchStrategy.GREEDY.toString())){
+			System.out.println("Starting engine with search strategy: " + "Greedy");
+			solver.engine(ohh1Problem, SearchStrategy.GREEDY);
+		}	
 		
 		
 	}
