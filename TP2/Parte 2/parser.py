76df31a6d07aceac93374df55ccca6f7f5346a78
mvalverd <mvalverd@alu.itba.edu.ar>
2016-05-17 19:26:15 -0300

    Actualizaciones de parser

--- TP2/Parte 2/parser.py
+++ TP2/Parte 2/parser.py
@@ -1,26 +1,42 @@
+import sys
 import re
+
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-def parseFile(infile):
+def parserFile(infile, number_of_values):
+    values = []
+    out_expected = []
+
+    i = 0
+    count = 0
+    dots = 0
+    limit = number_of_values
+    for line in open(infile, 'r'):
+        val = re.findall('[a-zA-Z0-9\.-]+', line)
+        if  i == 1 and  count < limit:
+            values.append([float(val[0]), float(val[1])])
+            out_expected.append(float(val[2]))
+            count = count + 1
+        if i == 1 and limit == -1:
+            values.append([float(val[0]), float(val[1])])
+            out_expected.append(float(val[2]))
+        else:
+            i = 1
+    return values,out_expected
+
+def plotX1X2Z(array_values, out_values):
     x1_vals = []
     x2_vals = []
-    z_vals = []
-    labels = 1
-    for line in open(infile, 'r'):
-        values = (re.findall('[a-zA-Z0-9\.-]+', line))
-        if labels == 0:
-            x1_vals.append(float(values[0]))
-            x2_vals.append(float(values[1]))
-            z_vals.append(float(values[2]))
-        else:
-            # no toma los labels
-            labels = 0
-    return x1_vals,x2_vals,z_vals
+    z_vals = out_values
 
-def plotX1X2Z(x1,x2,z):
+    for row in array_values:
+        x1_vals.append(row[0])
+        x2_vals.append(row[1])
+
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x1, x2, z, c='r', marker='o')
+    ax.scatter(x1_vals, x2_vals, z_vals, c='r', marker='o')
     ax.set_xlabel('X1')
     ax.set_ylabel('X2')
     ax.set_zlabel('Z')
@@ -28,5 +44,5 @@ def plotX1X2Z(x1,x2,z):
     return
 
 # print('archivo de entrada:', sys.argv[1])
-# x1, x2, z = parseFile(sys.argv[1])
-# plotX1X2Z(x1, x2, z)
+# valores, expected = parserFile(sys.argv[1], -1)
+# plotX1X2Z(valores, expected)
