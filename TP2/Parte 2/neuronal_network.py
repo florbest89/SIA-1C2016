b8a15f5a06edd3e022ed47492d9bcdede3d29047
Florencia Besteiro <florbest89@gmail.com>
2016-05-17 23:53:48 -0300

    Ultimo fix. Sigue error con exp

--- TP2/Parte 2/neuronal_network.py
+++ TP2/Parte 2/neuronal_network.py
@@ -3,13 +3,13 @@ import backpropagation as bp
 import matplotlib.pyplot as plt
 
 def learn():
-    inputs, outputs = fp.parse_file('terrain/terrain5-1.txt', 50)
-    errors, epoch = bp.multilayer_perceptron([2, 10, 5, 1], inputs,outputs, -1, 0.5, 0.7, 0.001, 'tan')
+    inputs, outputs = fp.parse_file('terrain/terrain5-4.txt', 50)
+    errors, epoch = bp.multilayer_perceptron([2, 10, 5, 1], inputs,outputs, -1, 0.5, 0.5, 0.005, 'tan')
 
     plt.plot(range(1, epoch), errors)
     plt.xlabel('Iteración')
     plt.ylabel('Error cuadrático medio')
-    plt.title('Red neuronal con arquitectura ' + str([2, 10, 5, 1]) + ', cantidad de patrones: 20, función de activación: ' + 'tan')
+    plt.title('Red neuronal con arquitectura ' + str([2, 10, 5, 1]) + ', cantidad de patrones: 20, función de activación: ' + 'exp')
     plt.show()
 
 learn()
