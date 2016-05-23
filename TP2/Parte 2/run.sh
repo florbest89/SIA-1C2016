c54d8b9c9460469623293084b6fa08604acacbff
Matias Rivas <rivasleandromatias@gmail.com>
2016-05-23 11:24:59 -0300

    ejemplo camino feliz

--- TP2/Parte 2/run.sh
+++ TP2/Parte 2/run.sh
@@ -5,4 +5,4 @@
 # want to do the full Sys V style init stuff.
 
 # Ejecutar el script de demo en cada arranque:
-python neuronal_network.py -hl1 10 -hl2 4 -G exp -ecm 0.1 -eta 0.0005 -alpha 0.9 -a 0.0001 -b 0.0001 -k 25
+python neuronal_network.py -hl1 10 -hl2 4 -G exp -ecm 0.0001 -eta 0.5 -alpha 0.9 -a 0.0001 -b 0.0001 -k 25
