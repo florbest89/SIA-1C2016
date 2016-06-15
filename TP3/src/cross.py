1370b7ec1dc09b8350d591dd1d8e074e8a495052
Florencia Besteiro <florbest89@gmail.com>
2016-06-15 13:46:14 -0300

    CROSS ANNULAR

--- TP3/src/cross.py
+++ TP3/src/cross.py
@@ -1,20 +1,20 @@
 from random import randint
 from models import *
+import math as m
 
 # TODO: IMPLEMENT CROSSES METHODS
 
 def cross_1P(p1, p2):
-    last = len(p1.items) + 1
+    L = len(p1.items) + 1
 
-    locus = randint(0,last)
+    locus = randint(0,len(p1.items))
     print('LOCUS : ' + str(locus))
 
-    return cross(p1,p2,locus,last)
-
+    return cross(p1,p2,locus,L)
 
 def cross_2P(p1,p2):
 
-    max_locus = len(p1.items) + 1
+    max_locus = len(p1.items)
 
     locus = randint(0, max_locus)
     locus2 = randint(0, max_locus)
@@ -29,22 +29,47 @@ def cross_2P(p1,p2):
     else:
         return cross_2P(p1,p2)
 
+def cross_annular(p1,p2):
+
+    L = len(p1.items) + 1
+
+    locus = randint(0,len(p1.items))
+    l = randint(1, int(m.floor(L / 2)))
+
+    print('LOCUS : ' + str(locus))
+    print('l : ' + str(l))
+
+    if locus + l <= L:
+        return cross(p1,p2,locus,locus + l)
+    else:
+        s1, s2 = cross(p1,p2,locus,L)
+
+        rest = locus + l - L
+        for i in range(0, rest):
+            allele1 = p1.get_allele(i)
+            print('padre 1: alelo locus: ' + str(i))
+            allele2 = p2.get_allele(i)
+            print('padre 2 alelo locus: ' + str(i))
+
+            s1.set_allele(i, allele2)
+            s2.set_allele(i, allele1)
+
+        return s1,s2
 
 def cross(p1,p2,locus_from,locus_to):
+
     s1 = p1.copy()
     s2 = p2.copy()
 
     for l in range(locus_from, locus_to):
         allele1 = p1.get_allele(l)
+        print('padre 1: alelo locus: ' + str(l))
         allele2 = p2.get_allele(l)
+        print('padre 2 alelo locus: ' + str(l))
 
         s1.set_allele(l, allele2)
         s2.set_allele(l, allele1)
 
     return s1, s2
 
-
-
 # TODO: UNIFORM CORSS (CROSSUNI)
-
-# TODO: ANNULAR CORSS (CROSSANN)
