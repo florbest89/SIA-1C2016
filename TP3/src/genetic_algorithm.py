0accb5a86c47c785828134a975f81d7dcb2944d8
Florencia Besteiro <florbest89@gmail.com>
2016-06-17 22:43:29 -0300

    CLASSIC MUTATION

--- TP3/src/genetic_algorithm.py
+++ TP3/src/genetic_algorithm.py
@@ -38,7 +38,7 @@ def genetic_algorithm():
 
     #son1, son2 = cross_annular(parent1,parent2)
 
-    mut = not_uniform(parent1)
+    mut = mutation(parent1, parent2, 0.5, 'classic')
 
     var = 2
 
--- TP3/src/mutation.py
+++ TP3/src/mutation.py
@@ -2,8 +2,18 @@ from models import *
 from random import randint, uniform
 from custom_parser import *
 
-# TODO: CLASSIC
 
+def classic(individual,p):
+    L = len(individual.items) + 1
+
+    for l in range(0,L):
+        r = uniform(0,1)
+
+        if r < p:
+            m = mutate_locus(l)
+            individual.set_allele(l, m)
+
+    return individual
 
 def not_uniform(individual):
 
@@ -11,19 +21,45 @@ def not_uniform(individual):
 
     locus = randint(0,L)
 
-    if locus == 0:
-        mutation = parse_helmets(1)[0]
-    elif locus == 1 :
-        mutation = parse_chestplates(1)[0]
-    elif locus == 2:
-        mutation = parse_gauntlets(1)[0]
-    elif locus == 3:
-        mutation = parse_weapons(1)[0]
-    elif locus == 4:
-        mutation = parse_boots(1)[0]
-    else:
-        mutation = uniform(1.3,2.0)
+    m = mutate_locus(locus)
 
-    individual.set_allele(locus,mutation)
+    individual.set_allele(locus,m)
 
     return individual
+
+def mutate_locus(locus):
+
+    if locus == 0:
+        return parse_helmets(1)[0]
+    elif locus == 1:
+        return parse_chestplates(1)[0]
+    elif locus == 2:
+        return parse_gauntlets(1)[0]
+    elif locus == 3:
+        return parse_weapons(1)[0]
+    elif locus == 4:
+        return parse_boots(1)[0]
+    else:
+        return uniform(1.3, 2.0)
+
+def mutation(c1, c2, pm, mutation_method):
+
+    p = uniform(0,1)
+
+    if p < pm:
+        c1 = mutate(c1, mutation_method)
+
+    p = uniform(0,1)
+
+    if p < pm:
+        c2 = mutate(c2,mutation_method)
+
+    return c1,c2
+
+def mutate(individual, mutation_method):
+
+    if mutation_method == 'classic':
+        return classic(individual, 0.5)
+
+    if mutation_method == 'not_uniform':
+        return not_uniform(individual)
